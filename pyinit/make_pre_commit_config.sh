340a8ff3526242b77dfa02d2941432dddfb433bd
hrsma2i <hrs.ma2i@gmail.com>
2021-07-28 18:05:13 +0900

    Add autoflake to pre-commit.

--- pyinit/make_pre_commit_config.sh
+++ pyinit/make_pre_commit_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 black_v=$(cat ./pyproject.toml | grep black | cut -d' ' -f3 | tr -d '^')
 flake8_v=$(cat ./pyproject.toml | grep flake8 | cut -d' ' -f3 | tr -d '^')
+autoflake_v=$(cat ./pyproject.toml | grep autoflake | cut -d' ' -f3 | tr -d '^')
 
 cat <<EOL >.pre-commit-config.yaml
 repos:
@@ -9,6 +10,15 @@ repos:
     hooks:
       - id: black
         language_version: python3
+  - repo: https://github.com/myint/autoflake
+    rev: $autoflake_v
+    hooks:
+      - id: autoflake
+        args:
+          - "--in-place"
+          - "--remove-all-unused-imports"
+          - "--ignore-init-module-imports"
+          - "--remove-unused-variables"
   - repo: https://github.com/pycqa/flake8
     rev: $flake8_v
     hooks:
