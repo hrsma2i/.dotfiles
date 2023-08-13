e37cb5839fa8a65a092ffdc21f15933e856380be
hrsma2i <hrs.ma2i@gmail.com>
2023-08-13 18:29:37 +0900

    init_typescript.sh

---
+++ init_typescript.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# https://ics.media/entry/4682/
+# https://typescript-jp.gitbook.io/deep-dive/nodejs
+# https://qiita.com/notakaos/items/3bbd2293e2ff286d9f49
+
+project_name=$1
+mkdir "$project_name"
+cd "$project_name" || exit
+npm init -y
+npm install --save-dev typescript
+npm install --save-dev @types/node
+npx tsc --init --rootDir src \
+    --outDir lib \
+    --esModuleInterop \
+    --resolveJsonModule \
+    --lib es6,dom \
+    --module commonjs
+npm install --save-dev ts-node
+npm install --save-dev nodemon
+gibo dump Node >.gitignore
