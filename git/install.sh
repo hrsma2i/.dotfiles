b1707a8832bc50b03488c15d7802134ed5f12464
hrsma2i <hrs.ma2i@gmail.com>
2025-02-19 10:23:26 +0900

    install git, gibo and pre-commit if they don't exit.

--- git/install.sh
+++ git/install.sh
@@ -4,13 +4,16 @@ TOOL_DIR=$(cd "$(dirname "$0")" && pwd)
 TOOL_NAME="$(basename "$TOOL_DIR")"
 DOTFILES_DIR="$(cd "$TOOL_DIR"/.. && pwd)"
 
+# shellcheck disable=SC1091
+source "$DOTFILES_DIR/shared/install_if_not_exists.sh"
+
 "$DOTFILES_DIR"/brew/install_itself.sh
 
 # Preinstalled git in MacOS is called `Apple Git`, which is not useful.
 # `brew git` has zsh completions.
-brew install git
-brew install gibo
-brew install pre-commit
+install_if_not_exists git 'brew install git'
+install_if_not_exists gibo 'brew install gibo'
+install_if_not_exists pre-commit 'brew install pre-commit'
 
 CONFIG_DIR="$HOME/.config/git"
 mkdir -p "$CONFIG_DIR"
